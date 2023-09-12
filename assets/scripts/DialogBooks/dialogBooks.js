9e95f76dcca3e10b5da11d361a89e4af654f367e
Paulo Freitas <1352010775@ulife.com.br>
2023-09-12 09:02:24 -0300

    teste

---
+++ assets/scripts/DialogBooks/dialogBooks.js
@@ -0,0 +1,24 @@
+/** ==================
+    AGREDECIMENTO PELO
+    CONTATO
+    ================== */
+
+var modal = document.querySelector("dialog")
+var btnClose = document.querySelector("dialog button")
+
+// SUGESTÃO DE LIVRO
+const btnSubmitBook = document.getElementById("submitBook")
+
+btnSubmitBook.onclick = function () {
+    const fieldsBooks = document.querySelectorAll(".fieldBooks") // CAMPOS
+
+    if ((fieldsBooks[0].value !== '') && (fieldsBooks[1].value !== '') && (fieldsBooks[2].value !== '')) {
+        modal.showModal()
+    }
+}
+
+// BOTÃO "OK" DO DIALOG
+btnClose.onclick = function () {
+    modal.close()
+}
+
